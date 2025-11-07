44fc0c436dc69658c198878e748409d4af19d9a8
CMF-Keph <carloskepht@gmail.com>
2025-11-07 21:08:06 +0100

    feat: navbar styling

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import { Lato } from "next/font/google";
-import "./globals.css";
+import "./globals.css"
 import Navbar from "./shared/Navbar";
 
 const font = Lato({
--- app/shared/Navbar.tsx
+++ app/shared/Navbar.tsx
@@ -1,8 +1,16 @@
+import Image from "next/image"
+import Link from "next/link"
+
 const Navbar = () => {
   return (
-    <div className="bg-gray-800 border-gray-700 border-b h-14 flex items-center px-4">
-      <h1 className="text-2xl text-gray-200 font-bold antialiased">Quizcard</h1>
-    </div>
+    <header className="bg-gray-800 border-gray-700 border-b h-20 flex items-center px-4">
+      <div className="max-w-7xl mx-auto w-full">
+        <Link href="/" className="flex gap-2 items-center group transition-transform">
+          <Image src="/logo.png" alt="Quizcard logo" width={48} height={48} className="object-contain"></Image>
+          <h1 className="text-2xl text-gray-100 font-bold antialiased group-hover:text-white">Quizcard</h1>
+        </Link>
+      </div>
+    </header>
   )
 }
 
